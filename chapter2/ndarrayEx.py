27e9398bb5b094f59ee7e3907914b2c7a016d39c
hadesyi <leesk55@gmail.com>
2022-12-14 12:13:05 +0900

    ndarray 객체 shape: ndarray의 (행,열) 크기 정보 ndim: 차원 정보 dtype: 데이터 타입 zero : 0으로 채워진 ndarray 리턴 ones : 1으로 채워진 ndarray 리턴 zeros,ones 함수의 인자로 튜플을 전달하면 0, 1로 구성된 다차원 배열을 생성할 수있다.
    넘파이의 arange 함수는 최대 세개의 파라미터를 입력할 수 있으며, 지정된 범위의 규칙적인 숫자를 갖는 ndarray 객체를 반환

--- chapter2/ndarrayEx.py
+++ chapter2/ndarrayEx.py
@@ -32,7 +32,12 @@ print(c.shape)
 print(np.zeros(3))
 print(np.ones(3))
 
-#zeros,ones 함수의 인자로 튜플을 전달하면 0, 1로 구성된 다차원 배열을 생성할 수있다.
+# zeros,ones 함수의 인자로 튜플을 전달하면 0, 1로 구성된 다차원 배열을 생성할 수있다.
 size = (3, 4)
 print(np.zeros(size))
 print(np.zeros((3, 4)))
+
+# 넘파이의 arange 함수는 최대 세개의 파라미터를 입력할 수 있으며, 지정된 범위의 규칙적인 숫자를 갖는 ndarray 객체를 반환
+print(np.arange(5))
+print(np.arange(1, 5))
+print(np.arange(1, 5, 2))
