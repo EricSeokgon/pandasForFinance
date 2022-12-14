e95e732f4df098266f382191756705dca1868a67
hadesyi <leesk55@gmail.com>
2022-12-14 18:13:30 +0900

    시리즈 연산

--- chapter3/seriesOp.py
+++ chapter3/seriesOp.py
@@ -6,3 +6,9 @@ from pandas import Series
 가족 = 철수 + 영희
 print(가족)
 print(철수 * 10)
+
+high = Series([42800, 42700, 42050, 42950, 43000])
+low = Series([42150, 42150, 41300, 42150, 42350])
+
+diff = high - low
+print(diff)
