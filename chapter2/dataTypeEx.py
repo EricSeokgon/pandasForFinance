c1bc8a118c0dffce7e5fa0a0d216929ba5863697
hadesyi <leesk55@gmail.com>
2022-12-14 15:45:49 +0900

    b의 데이터 타입 int8은 -128~127까지만 표현

--- chapter2/dataTypeEx.py
+++ chapter2/dataTypeEx.py
@@ -8,3 +8,11 @@ print(arr.dtype.alignment)
 # astype 메서드를 사용해서 기존의 ndarray를 다른 타입으로 변경할 수 있다.
 arr = arr.astype('int8')
 print(arr.dtype)
+
+# b의 데이터 타입 int8은 -128~127까지만 표현
+a = np.array([0, 255], dtype=np.uint8)
+b = a.astype('int8')
+print(a)
+print(b)
+
+
