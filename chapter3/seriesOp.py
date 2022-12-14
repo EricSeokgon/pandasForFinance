345908ae24f4b0786e56567f3c967f2f6c82b669
hadesyi <leesk55@gmail.com>
2022-12-14 18:09:51 +0900

    시리즈 연산

--- chapter3/seriesOp.py
+++ chapter3/seriesOp.py
@@ -1,6 +1,8 @@
 from pandas import Series
+
 # 시리즈 연산
 철수 = Series([10, 20, 30], index=['NAVER', 'SKT', 'KT'])
 영희 = Series([10, 20, 30], index=['SKT', 'KT', 'NAVER'])
 가족 = 철수 + 영희
 print(가족)
+print(철수 * 10)
