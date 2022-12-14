aa05e78b365730642d8f52d75677851385d134bb
hadesyi <leesk55@gmail.com>
2022-12-14 18:05:32 +0900

    시리즈 수정/추가/삭제 값 수정

--- chapter3/seriesEdit.py
+++ chapter3/seriesEdit.py
@@ -4,5 +4,7 @@ data = [1000, 2000, 3000]
 index = ["메로나", "구구콘", "하겐다즈"]
 s = Series(data=data, index=index)
 
-s.loc['메로나']=500 # 값 수정
+s.loc['메로나'] = 500  # 값 수정
 print(s)
+s.iloc[0] = 500  # iloc 연산 사용
+s['메로나'] = 500  # []기호 사용
