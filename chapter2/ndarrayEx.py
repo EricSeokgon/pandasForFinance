4d90a53b1d9f41bc77e076630ae3ecb6a772d14e
hadesyi <leesk55@gmail.com>
2022-12-14 11:54:05 +0900

    shape: ndarray의 (행,열) 크기 정보 ndim: 차원 정보 dtype: 데이터 타입

--- chapter2/ndarrayEx.py
+++ chapter2/ndarrayEx.py
@@ -14,3 +14,17 @@ arr2 = np.array(data2)
 print(arr2)
 print(type(arr2))
 print(type(arr2[0]))
+print(arr2.shape)
+print(arr2.ndim)
+print(arr2.dtype)
+print(arr1.shape)
+
+data = [
+    [1],
+    [2],
+    [3],
+    [4]
+]
+
+c = np.array(data)
+print(c.shape)
