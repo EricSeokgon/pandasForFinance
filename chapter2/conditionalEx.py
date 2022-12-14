aee66f7524626e0a5b90573706bf7e8d10a6b132
hadesyi <leesk55@gmail.com>
2022-12-14 16:40:20 +0900

    연산자 우선순위가 대소 비교보다 &가 높기 때문에 대소 비교 연산을 먼저 수행하기 위해 괄호로 조건을 반드시 묶어야 한다.

--- chapter2/conditionalEx.py
+++ chapter2/conditionalEx.py
@@ -8,3 +8,14 @@ print(arr > 10)
 arr = np.array([10, 20, 30])
 cond = [False, True, True]
 print(arr[cond])
+
+# 두개의 조건을 비교하는 예제
+arr = np.array([10, 20, 30])
+cond0 = arr > 10
+cond1 = arr < 30
+print(arr[cond0 & cond1])
+
+# 연산자 우선순위가 대소 비교보다 &가 높기 때문에 대소 비교 연산을 먼저 수행하기 위해 괄호로 조건을 반드시 묶어야 한다.
+arr = np.array([10, 20, 30])
+print(arr[(arr > 10) & (arr < 30)])
+
