38eea4fe8d281699c521cdeba2bbc21b05857d3d
hadesyi <leesk55@gmail.com>
2022-12-14 17:59:12 +0900

    iloc[시작 행번호:끝 행번호], loc[시작 인덱스: 끝 인덱스]

--- chapter3/seriesSlicingEx.py
+++ chapter3/seriesSlicingEx.py
@@ -4,4 +4,7 @@ from pandas import Series
 data = [1000, 2000, 3000]
 index = ["메로나", "구구콘", "하겐다즈"]
 s = Series(data=data, index=index)
+
+# iloc[시작 행번호:끝 행번호], loc[시작 인덱스: 끝 인덱스]
 print(s.iloc[0:2])
+print(s.loc['메로나':'구구콘'])
