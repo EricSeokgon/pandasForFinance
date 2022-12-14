27e94ee318524f454d49a16290ee54d925bcc60e
hadesyi <leesk55@gmail.com>
2022-12-14 17:40:28 +0900

    딕셔너리 객체에서 keys와 values 메서드를 사용하는 것과 유사하게 시리즈 객체는 index와 values라는 속성을 통해 인덱스와 값을 각각 분리해서 얻어올수 있다.

--- chapter3/dailyPrice.py
+++ chapter3/dailyPrice.py
@@ -16,3 +16,8 @@ data = {
 }
 
 s = Series(data)
+
+# 딕셔너리 객체에서 keys와 values 메서드를 사용하는 것과 유사하게 시리즈 객체는 index와 values라는 속성을 통해 인덱스와 값을 각각 분리해서 얻어올수 있다.
+print(s.index)
+print(s.index.dtype)
+print(s.values)
