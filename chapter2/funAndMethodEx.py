ba0806e3519ff21b7d77ea1ef6b44762a2c4daca
hadesyi <leesk55@gmail.com>
2022-12-14 16:56:08 +0900

    size 옵션을 사용하면 생성되는 숫자의 개수를 지정할 수있다.

--- chapter2/funAndMethodEx.py
+++ chapter2/funAndMethodEx.py
@@ -7,3 +7,10 @@ print(arr.sum())
 # sum메서드에 축정보를 지정할 수있다.
 print(arr.sum(axis=0))
 print(arr.sum(axis=1))
+
+# 넘파이를 사용해서 임의의 숫자를 만들 수도 있다.
+np.random.randint(3)
+
+# size 옵션을 사용하면 생성되는 숫자의 개수를 지정할 수있다.
+np.random.randint(46, size=5)
+np.random.randint(46, size=(2,5))
