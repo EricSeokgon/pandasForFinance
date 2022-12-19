3e95abf63400e97bc27045250d39e4c6bcd66cac
hadesyi <leesk55@gmail.com>
2022-12-19 15:47:56 +0900

    시리즈 필터링 시가와 종가를 시리즈 객체로 표현한 후 종가가 시가보다 큰지를 비교

--- chapter3/seriesFilterEx.py
+++ chapter3/seriesFilterEx.py
@@ -5,5 +5,13 @@ index = ['2019-05-31', '2019-05-30', '2019-05-29', '2019-05-28', '2019-05-27']
 s = Series(data=data, index=index)
 cond = s > 4200
 print(cond)
-
 print(s[cond])
+
+close = [42500, 42550, 41800, 42550, 42650]
+open = [42600, 42200, 41850, 42550, 42500]
+index = ['2019-05-31', '2019-05-30', '2019-05-29', '2019-05-28', '2019-05-27']
+
+open = Series(data=open, index=index)
+close = Series(data=close, index=index)
+cond = close > open
+print(cond)
