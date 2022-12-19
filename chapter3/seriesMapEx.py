3446eb6aa7b0cb251f6cfe0f1c4f1a1826fb14e7
hadesyi <leesk55@gmail.com>
2022-12-19 15:38:09 +0900

    시리즈 Map map 메서드를 사용해서 수치형 데이터를 범주형 데이터터로 구분

--- chapter3/seriesMapEx.py
+++ chapter3/seriesMapEx.py
@@ -4,6 +4,19 @@ from pandas import Series
 def remove_comma(x):
     return int(x.replace(",", ""))
 
+
 s = Series(["1,234", "5,678", "9,876"])
 result = s.map(remove_comma)
 print(result)
+
+
+def is_greater_than_5000(x):
+    if x > 5000:
+        return "크다"
+    else:
+        return "작다"
+
+
+s = Series([1234, 5678, 9876])
+s = s.map(is_greater_than_5000)
+print(s)
