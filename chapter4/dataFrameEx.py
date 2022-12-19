2ea51d785d207df2920166d8f685e3c7fba55b14
hadesyi <leesk55@gmail.com>
2022-12-19 16:19:20 +0900

    행단위로 데이터를 표현 컬럼의 이름을 딕셔너리의 키로 데이터를 딕셔너리의 값으로 사용

--- chapter4/dataFrameEx.py
+++ chapter4/dataFrameEx.py
@@ -17,3 +17,11 @@ data = [
 columns = ["종목코드", "종목명", "현재가"]
 df = DataFrame(data=data, columns=columns)
 print(df)
+
+data = [
+    {"종목코드": "037730", "종목명": "3R", "현재가": 1510},
+    {"종목코드": "06360", "종목명": "3SOFT", "현재가": 1790},
+    {"종목코드": "005760", "종목명": "ACTS", "현재가": 1185}
+]
+df = DataFrame(data=data)
+print(df)
