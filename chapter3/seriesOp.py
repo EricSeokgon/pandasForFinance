b026056ce0bfebd417eb36b91e130cb77f9d953b
hadesyi <leesk55@gmail.com>
2022-12-19 15:24:03 +0900

    시리즈 연산 시리즈는 최댓값일 때의 인덱스를 반환하는 idxmax와 최솟값일 때의 인덱스를 반환하는 dixmin메서드를 제공함

--- chapter3/seriesOp.py
+++ chapter3/seriesOp.py
@@ -19,3 +19,15 @@ high = Series([42800, 42700, 42050, 42950, 43000], index=date)
 low = Series([42150, 42150, 41300, 42150, 42350], index=date)
 diff = high - low
 print(diff)
+
+max_idx = 0
+max_val = 0
+for i in range(len(diff)):
+    if diff[i] > max_val:
+        max_val = diff[i]
+        max_idx = i
+print(max_idx)
+print(diff.index[max_idx])
+
+print(diff.idxmax())
+print(diff.idxmin())
