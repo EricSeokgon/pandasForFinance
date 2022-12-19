6d73646a09ede8997017293094d5f4f07730ae41
hadesyi <leesk55@gmail.com>
2022-12-19 15:42:22 +0900

    시리즈 필터링 시리즈 객체에 비교 연산을 적용

---
+++ chapter3/seriesFilterEx.py
@@ -0,0 +1,7 @@
+from pandas import Series
+
+data = [42500, 42550, 41800, 42550, 42650]
+index = ['2019-05-31', '2019-05-30', '2019-05-29', '2019-05-28', '2019-05-27']
+s = Series(data=data, index=index)
+cond = s > 4200
+print(cond)
