9f816c61fb7fe93edb96699e5164d91914d1220a
hadesyi <leesk55@gmail.com>
2022-12-19 15:52:51 +0900

    시리즈 필터링 종가가 시가보다 높은 날의 거래일과 변동폭을 출력

--- chapter3/seriesFilterEx.py
+++ chapter3/seriesFilterEx.py
@@ -20,3 +20,12 @@ print(close[close > open])  # 변수를 사용하지 않고 한줄로 짧게 표
 
 print(close.index[close > open])
 print(close[close > open].index)
+
+close = [42500, 42550, 41800, 42550, 42650]
+open = [42600, 42200, 41850, 42550, 42500]
+index = ['2019-05-31', '2019-05-30', '2019-05-29', '2019-05-28', '2019-05-27']
+
+open = Series(data=open, index=index)
+close = Series(data=close, index=index)
+diff = close - open
+print(diff[close > open])
