b5bc4a05a9d5d3d926308731379064f3f6a4c99d
hadesyi <leesk55@gmail.com>
2022-12-21 11:13:54 +0900

    컬럼 인덱싱 데이터프레임은 2차원 자료구조로 로우 또는 컬럼 단위 모두 인덱싱이 가능

---
+++ chapter4/columnsIndexingEx.py
@@ -0,0 +1,12 @@
+from pandas import DataFrame
+
+data = [
+    ["3R", 1510, 7.36],
+    ["3SOFT", 1790, 1.65],
+    ["ACTS", 1185, 1.28],
+]
+
+index = ["037730", "036360", "005760"]
+columns = ["종목명", "현재가", "등락률"]
+df = DataFrame(data=data, index=index, columns=columns)
+print(df['현재가'])
