0f9ad8141e26a442ba3764c4bf68b9eb91e58d24
hadesyi <leesk55@gmail.com>
2022-12-21 11:28:31 +0900

    loc 속성에 로우 인덱스와 컬럼명을 순서대로 입력하면 특정 로우와 컬럼에 위치하는 값을 한번에 가져올수 있습니다.

--- chapter4/columnsIndexingEx.py
+++ chapter4/columnsIndexingEx.py
@@ -9,22 +9,25 @@ data = [
 index = ["037730", "036360", "005760"]
 columns = ["종목명", "현재가", "등락률"]
 df = DataFrame(data=data, index=index, columns=columns)
-print(df['현재가']) #데이터 프레임의 인덱싱
+print(df['현재가'])  # 데이터 프레임의 인덱싱
 print(df.현재가)
-리스트 = ["현재가","등락률"]
+리스트 = ["현재가", "등락률"]
 print(df[리스트])
-print(df[["현재가","등락률"]])
-print(df[['현재가']]) # 데이터프레임의 한개 컬럼 슬라이싱
+print(df[["현재가", "등락률"]])
+print(df[['현재가']])  # 데이터프레임의 한개 컬럼 슬라이싱
 print(df.loc["037730"])
 print(df.iloc[0])
 print(df.iloc[-1])
 
-#행 번호로 행 선태 후 시리즈 인덱싱
-print(df.iloc[0].iloc[1]) #시리즈 행 번호
-print(df.iloc[0].loc["현재가"]) #시리즈 인덱스
-print(df.iloc[0]["현재가"]) #시리즈 인덱스
+# 행 번호로 행 선태 후 시리즈 인덱싱
+print(df.iloc[0].iloc[1])  # 시리즈 행 번호
+print(df.iloc[0].loc["현재가"])  # 시리즈 인덱스
+print(df.iloc[0]["현재가"])  # 시리즈 인덱스
 
-#인덱스로 행 선택 후 시리즈 인덱싱
-print(df.loc["037730"].iloc[1]) #시리즈 행 번호
-print(df.loc["037730"].loc["현재가"]) #시리즈 인덱스
-print(df.loc["037730"]["현재가"]) #시리즈 인덱스
+# 인덱스로 행 선택 후 시리즈 인덱싱
+print(df.loc["037730"].iloc[1])  # 시리즈 행 번호
+print(df.loc["037730"].loc["현재가"])  # 시리즈 인덱스
+print(df.loc["037730"]["현재가"])  # 시리즈 인덱스
+
+print(df.loc["037730", "현재가"])
+print(df.iloc[0, 1])
