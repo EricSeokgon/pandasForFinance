17878d578184bb9a64b097870c260e6ae1b61554
hadesyi <leesk55@gmail.com>
2022-12-21 11:19:08 +0900

    컬럼 인덱싱 print(df['현재가']) #데이터 프레임의 인덱싱 print(df[['현재가']]) # 데이터프레임의 한개 컬럼 슬라이싱

--- chapter4/columnsIndexingEx.py
+++ chapter4/columnsIndexingEx.py
@@ -9,8 +9,9 @@ data = [
 index = ["037730", "036360", "005760"]
 columns = ["종목명", "현재가", "등락률"]
 df = DataFrame(data=data, index=index, columns=columns)
-print(df['현재가'])
+print(df['현재가']) #데이터 프레임의 인덱싱
 print(df.현재가)
 리스트 = ["현재가","등락률"]
 print(df[리스트])
 print(df[["현재가","등락률"]])
+print(df[['현재가']]) # 데이터프레임의 한개 컬럼 슬라이싱
