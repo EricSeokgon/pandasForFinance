19b6dac95ed94d8ae210e8dc07ecd358e273b1b1
hadesyi <leesk55@gmail.com>
2023-01-26 17:34:34 +0900

    컬럼 레이블 변경 생성된 데이터프레임 객체에서 컬럼 레이블과 인덱스의 이름을 수정하는 방법

--- chapter4/columnLableModfiyEx.py
+++ chapter4/columnLableModfiyEx.py
@@ -9,7 +9,12 @@ data = [
 index = ["037730", "036360", "005760"]
 columns = ["종목명", "현재가", "등락률"]
 df = DataFrame(data=data, index=index, columns=columns)
-print(df.columns)
-print(df.index)
+df.rename(columns={'종목명': 'code'}, inplace=True)
+print(df)
 
+# print(df.columns)
+# print(df.index)
 
+# df.columns = ['name', 'close', 'fluctuation']  # 컬럼 이름 변경
+# df.index.name = 'code'
+# print(df)
