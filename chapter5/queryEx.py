679d0df1673b47b9d60af20ef4cf2a83226bb608
hadesyi <leesk55@gmail.com>
2023-01-27 09:56:48 +0900

    Query 데이터프레임 생성

--- chapter5/queryEx.py
+++ chapter5/queryEx.py
@@ -1,10 +1,10 @@
 from pandas import DataFrame
 
 data = [
-    {"cd": "A060310", "nm": "3S", "close": "2,920"},
-    {"cd": "A095570", "nm": "AJ네트웍스", "close": "6,250"},
-    {"cd": "A006840", "nm": "AK홀딩스", "close": "29,700"},
-    {"cd": "A054620", "nm": "APS홀딩스", "close": "19,400"}
+    {"cd": "A060310", "nm": "3S", "open": 2920, "close": 2800},
+    {"cd": "A095570", "nm": "AJ네트웍스", "open": 1920, "close": 1900},
+    {"cd": "A006840", "nm": "AK홀딩스", "open": 2020, "close": 2010},
+    {"cd": "A054620", "nm": "APS홀딩스", "open": 3120, "close": 3200}
 ]
 df = DataFrame(data=data)
 df = df.set_index('cd')
