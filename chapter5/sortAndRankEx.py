68740da62b11869eb3e669dab87d47955724fbad
hadesyi <leesk55@gmail.com>
2023-01-27 15:53:41 +0900

    정렬및순위 데이터프레임 오름차순 정렬

---
+++ chapter5/sortAndRankEx.py
@@ -0,0 +1,14 @@
+from pandas import DataFrame
+
+data = [
+    ["037730", "3R", 1510],
+    ["036360", "3SOFT", 1790],
+    ["005670", "ACTS", 1185]
+]
+
+columns = ["종목코드", "종목명", "현재가"]
+df = DataFrame(data=data, columns=columns)
+df.set_index("종목코드", inplace=True)
+
+df2 = df.sort_values(by="현재가")
+print(df2)
