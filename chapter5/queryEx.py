44d88ee47dfbe0655008b5e3dc6b1537d3f869ec
hadesyi <leesk55@gmail.com>
2023-01-27 09:51:31 +0900

    Query 데이터프레임 생성

---
+++ chapter5/queryEx.py
@@ -0,0 +1,11 @@
+from pandas import DataFrame
+
+data = [
+    {"cd": "A060310", "nm": "3S", "close": "2,920"},
+    {"cd": "A095570", "nm": "AJ네트웍스", "close": "6,250"},
+    {"cd": "A006840", "nm": "AK홀딩스", "close": "29,700"},
+    {"cd": "A054620", "nm": "APS홀딩스", "close": "19,400"}
+]
+df = DataFrame(data=data)
+df = df.set_index('cd')
+print(df)
