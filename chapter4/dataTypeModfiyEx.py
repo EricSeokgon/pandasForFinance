3aca72d1c9f5f1a7bce875530607653994e56986
hadesyi <leesk55@gmail.com>
2023-01-27 09:42:13 +0900

    데이터 타입 변경 remove_comma 함수를 정의 replace 메서드로 콤마 제거 applymap 메서드로 중복 코드 제거 astype 메서드로 형변환

--- chapter4/dataTypeModfiyEx.py
+++ chapter4/dataTypeModfiyEx.py
@@ -1,22 +1,19 @@
+import numpy as np
 from pandas import DataFrame
-
-data = [
-    ["1,000", "1,100", "1,510"],
-    ["1,410", "1,420", "1,790"],
-    ["850", "900", "1,185"]
-]
-columns = ["03/02", "03/03", "03/04"]
-df = DataFrame(data=data, columns=columns)
-print(df)
 
 
 def remove_comma(x):
     return int(x.replace(',', ''))
 
 
-# df['03/02'] = df['03/02'].map(remove_comma)
-# df['03/03'] = df['03/03'].map(remove_comma)
-# df['03/04'] = df['03/04'].map(remove_comma)
+data = [
+    ["1,000", "1,100", "1,510"],
+    ["1,410", "1,420", "1,790"],
+    ["850", "900", "1,185"]
+]
+
+columns = ["03/02", "03/03", "03/04"]
+df = DataFrame(data=data, columns=columns)
 df = df.applymap(remove_comma)
-print(df)
+df = df.astype({'03/02': np.int64, '03/03': np.int64, '03/04': np.int64})
 print(df.dtypes)
