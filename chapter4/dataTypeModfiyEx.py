a7e2cc32d3f932a7c9f75f92731f6c6ea4868978
hadesyi <leesk55@gmail.com>
2023-01-27 09:37:34 +0900

    데이터 타입 변경 remove_comma 함수를 정의 replace 메서드로 콤마 제거

--- chapter4/dataTypeModfiyEx.py
+++ chapter4/dataTypeModfiyEx.py
@@ -10,3 +10,11 @@ df = DataFrame(data=data, columns=columns)
 print(df)
 
 
+def remove_comma(x):
+    return int(x.replace(',', ''))
+
+df['03/02'] = df['03/02'].map(remove_comma)
+df['03/03'] = df['03/03'].map(remove_comma)
+df['03/04'] = df['03/04'].map(remove_comma)
+
+print(df)
