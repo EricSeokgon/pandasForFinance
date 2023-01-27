b6c8fa52861177f451b8cfb45febf73d32464e59
hadesyi <leesk55@gmail.com>
2023-01-27 16:01:59 +0900

    인덱스 연산 판다스는 브로드캐스팅이 적용되어 반복문을 사용하지 않고도 전체 데이터 연산을 확장 적용할 수 있습니다.

--- chapter5/indexOpEx.py
+++ chapter5/indexOpEx.py
@@ -4,5 +4,8 @@ idx1 = pd.Index([1, 2, 3])
 idx2 = pd.Index([2, 3, 4])
 print(type(idx1))
 
+idx1.union(idx2) # 합집합
 
+idx1.intersection(idx2) # 중복된 데이터만 선택하고 싶다면 intersection 메서드를 사용한다. 교집합
 
+idx1.difference(idx2) # idx1에서 idx2와 중복되는 인덱스 값만 제거하고 싶을때 집합에서 차집합과 같다.
