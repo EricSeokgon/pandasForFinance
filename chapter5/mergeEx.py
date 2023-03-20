f122f56b0198180d666b16f1ffcb41bbd97a645b
hadesyi <leesk55@gmail.com>
2023-03-20 13:40:50 +0900

    Merge 두 데이터프레임의 컬럼 이름이 다르기 때문에 합칠 기준이되는 각 컬럼의 이름을 모두 입력해야 한다. left_on과 right_on 파라미터에 두 컬럼 이름을 넣는다.

--- chapter5/mergeEx.py
+++ chapter5/mergeEx.py
@@ -5,7 +5,7 @@ import pandas as pd
 data = [
     ["전기전자", "005930", "삼성전자", 74400],
     ["화학", "051910", "LG화학", 896000],
-    ["전기전자", "000660", "SK하이닉스", 101500]
+    ["서비스업", "035720", "카카오", 121500]
 ]
 
 columns = ["업종", "종목코드", "종목명", "현재가"]
@@ -19,7 +19,7 @@ data = [
     ["전기전자", -2.43]
 ]
 
-columns = ["업종", "등락률"]
+columns = ["항목", "등락률"]
 df2 = DataFrame(data=data, columns=columns)
-df = pd.merge(left=df1, right=df2, on='업종')
+df = pd.merge(left=df1, right=df2, left_on='업종', right_on='항목')
 print(df)
