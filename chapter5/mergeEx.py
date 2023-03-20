90c1c2351544de1f81a4aef1b16c574d04a43f11
hadesyi <leesk55@gmail.com>
2023-03-20 13:29:15 +0900

    Merge 판다스의 merge는 데이터프레임을 병합한다. concat이 단순히 두 데이터프레임을 이어 붙이는 연결이면 merge는 특정 컬럼의 값을 기준으로 데이터를 병합한다.

---
+++ chapter5/mergeEx.py
@@ -0,0 +1,25 @@
+from pandas import DataFrame
+import pandas as pd
+
+# 첫 번째 데이터프레임
+data = [
+    ["전기전자", "005930", "삼성전자", 74400],
+    ["화학", "051910", "LG화학", 896000],
+    ["전기전자", "000660", "SK하이닉스", 101500]
+]
+
+columns = ["업종", "종목코드", "종목명", "현재가"]
+df1 = DataFrame(data=data, columns=columns)
+
+# 두 번째 데이터프레임
+data = [
+    ["은행", 2.92],
+    ["보험", 0.37],
+    ["화학", 0.06],
+    ["전기전자", -2.43]
+]
+
+columns = ["업종", "등락률"]
+df2 = DataFrame(data=data, columns=columns)
+df = pd.merge(left=df1, right=df2, on='업종')
+print(df)
